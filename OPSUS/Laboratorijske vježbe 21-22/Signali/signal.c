e2b80475b16759e9e05870c4cdef20ea5aea1f82
maki2707 <filip.marcec@fer.hr>
2022-03-23 17:55:58 +0100

    dodan prvi labos

---
+++ OPSUS/Laboratorijske vježbe 21-22/Signali/signal.c
@@ -0,0 +1,99 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <math.h>
+
+void obradi_dogadjaj(int sig);
+void obradi_sigterm(int sig);
+void obradi_sigint(int sig);
+
+int broj; /*ovdje spremam broj koji na početku pročitam iz statusa*/
+int obrada_broj = 0;
+int nije_kraj = 1;
+FILE *fileStatus;
+FILE *fileObrada;
+
+int main()
+{
+struct sigaction act;
+ /* 1. maskiranje signala SIGUSR1 */
+ act.sa_handler = obradi_dogadjaj; /* kojom se funkcijom signal obrađuje */
+ sigemptyset(&act.sa_mask);
+ sigaddset(&act.sa_mask, SIGTERM); /* blokirati i SIGTERM za vrijeme obrade */
+ act.sa_flags = 0; /* naprednije mogućnosti preskočene */
+ sigaction(SIGUSR1, &act, NULL); /* maskiranje signala preko sučelja OS-a */
+ /* 2. maskiranje signala SIGTERM */
+ act.sa_handler = obradi_sigterm;
+ sigemptyset(&act.sa_mask);
+ sigaction(SIGTERM, &act, NULL);
+ /* 3. maskiranje signala SIGINT */
+ act.sa_handler = obradi_sigint;
+ sigaction(SIGINT, &act, NULL);
+ printf("Program s PID=%ld krenuo s radom\n", (long) getpid());
+
+    
+    
+
+    char br[20];  
+    fileObrada = fopen("obrada.txt", "r+");
+    fileStatus = fopen("status.txt", "r+");
+    fscanf(fileStatus,"%d", &broj);
+    fclose(fileStatus);
+    if(broj == 0)
+    {
+        while (fgets(br, sizeof(br)+1, fileObrada) != NULL)
+        {
+            sscanf(br, "%d", &obrada_broj);
+            obrada_broj = sqrt(obrada_broj);
+        }        
+    }
+    else 
+    {
+        /*ako u statusu nije 0, tj. ako nije došlo do prekida*/
+        obrada_broj = broj;
+    }
+    fclose(fileObrada);
+
+    /*zapiši 0 u status.txt na početak datoteke, prebrisat ću ono što je bilo -> w+*/
+    fileStatus = fopen("status.txt", "w+");
+    fprintf(fileStatus,"%d",0);
+    fclose(fileStatus);
+
+    int x = 0;
+    fileObrada = fopen("obrada.txt", "a+");
+    fprintf(fileObrada,"\n");
+    while(nije_kraj)
+    {
+        obrada_broj++;
+        x = obrada_broj*obrada_broj;
+        fprintf(fileObrada,"%d \n", x);
+        printf("%d \n", x);
+        fflush(fileObrada);
+        sleep(5);
+    }
+    
+    fclose(fileObrada);    
+}
+
+void obradi_dogadjaj(int sig)
+{ 
+    printf("Trenutni broj u obradi je : %d  \n", obrada_broj);
+}
+
+void obradi_sigterm(int sig)
+{ 
+    fileStatus = fopen("status.txt", "w+");
+    fprintf(fileStatus,"%d", obrada_broj);
+    fclose(fileStatus);    
+    printf("Primio signal SIGTERM, pospremam %d prije izlaska iz programa\n", obrada_broj);    
+    exit(1);
+}
+
+void obradi_sigint(int sig)
+{
+    printf("Primio signal SIGINT, prekidam rad programa...END\n");
+    exit(1);
+}
